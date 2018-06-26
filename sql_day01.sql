520be34a20cedc8a7171a55e17af09165b7f7e8c
kayh45 <kayh4545@gmail.com>
2018-06-26 15:30:22 +0900

    수업 내용 3시 30분 커밋

--- sql_day01.sql
+++ sql_day01.sql
@@ -170,3 +170,78 @@ SELECT e.EMPNO
   FROM emp e
  ORDER BY e.COMM, e.JOB, e.ENAME DESC
 ;
+
+-- (4) Alias : 별칭
+--  10) emp 테이블에서 아래 각 컬럼에 별칭을 풀네임을 주어서 조회
+--      empno --> Employee No.
+--      ename --> Employee Name
+--      job   --> Job Name
+SELECT e.EMPNO AS "Employee No."
+     , e.ENAME AS "Employee Name"
+     , e.JOB AS "Job Name"
+  FROM emp e
+;
+
+--  11) 10번과 동일 as 키워드 생략하여 조회
+--      empno --> 사번
+--      ename --> 사원 이름
+--      job   --> 직무
+SELECT e.EMPNO 사번
+     , e.ENAME "사원 이름"
+     , e.JOB "직무"
+  FROM emp e
+;
+
+--  12) 테이블에 붙이는 별칭
+SELECT empno
+  FROM emp
+;
+
+SELECT emp.empno
+  FROM emp
+;
+
+SELECT e.empno -- FROM 절에서 설정된 테이블 별칭은 SELECT 절에서 사용됨.
+  FROM emp e -- 소문자 e가 emp 테이브르이 별칭이며 테이블 별칭은 FROM 절에 사용함
+;
+
+SELECT d.deptno
+  FROM dept d
+;
+
+--  13) 영문 별칭 사용 시 특수기호 _ 사용하는 경우
+SELECT e.EMPNO Employee_No
+     , e.ENAME "Employee Name"
+  FROM emp e
+;
+
+--  14) 별칭과 정렬의 조합 : SELECT 절에 별칭을 준 경우 ORDER BY 절에서 사용가능
+--      emp 테이블에서 사번, 이름, 직무, 입사일, 커미션을 조회할 때
+--      각 컬럼에 대해서 한글 별칭을 주어 조회
+--      정렬은 커미션, 직무, 이름을 오름차순으로 정렬
+SELECT e.EMPNO    사번
+     , e.ENAME    이름
+     , e.JOB      직무
+     , e.HIREDATE 입사일
+     , e.COMM     커미션
+  FROM emp e
+ ORDER BY 커미션, 직무, 이름
+;
+
+--  15) DISTINCT, 별칭, 정렬의 조합
+--      job을 중복을 제거하여 직무라는 별칭을 조회하고
+--      내림차순으로 정렬
+SELECT DISTINCT 
+       e.JOB AS "직무"
+  FROM emp e
+ ORDER BY 직무 DESC
+;
+/*
+  직무
+---------
+SALESMAN
+PRESIDENT
+MANAGER
+CLERK
+ANALYST
+*/
--- 강현lab01.sql
+++ 강현lab01.sql
@@ -97,3 +97,79 @@ SELECT e.EMPNO AS "사번"
 7499	ALLEN	300
 7844	TURNER	0
 */
+
+
+--실습 5)
+SELECT e.EMPNO AS "사번"
+     , e.ENAME AS "이름"
+     , e.SAL AS "급여"
+     , e.HIREDATE AS "입사일"
+  FROM emp e
+;
+/*
+사번     이름   급여      입사일
+-------------------------------
+7369	SMITH	800 	80/12/17
+7499	ALLEN	1600	81/02/20
+7521	WARD	1250	81/02/22
+7566	JONES	2975	81/04/02
+7654	MARTIN	1250	81/09/28
+7698	BLAKE	2850	81/05/01
+7782	CLARK	2450	81/06/09
+7839	KING	5000	81/11/17
+7844	TURNER	1500	81/09/08
+7900	JAMES	950 	81/12/03
+7902	FORD	3000	81/12/03
+7934	MILLER	1300	82/01/23
+*/
+
+-- 실습 6) 
+SELECT *
+  FROM emp e
+;
+/*
+EMPNO   ENAME    JOB        MGR     HIREDATE    SAL    COMM   DEPTNO
+---------------------------------------------------------------------
+7369	SMITH	CLERK	    7902	80/12/17	800		        20
+7499	ALLEN	SALESMAN	7698	81/02/20	1600	300	    30
+7521	WARD	SALESMAN	7698	81/02/22	1250	500	    30
+7566	JONES	MANAGER 	7839	81/04/02	2975		    20
+7654	MARTIN	SALESMAN	7698	81/09/28	1250	1400	30
+7698	BLAKE	MANAGER	    7839	81/05/01	2850		    30
+7782	CLARK	MANAGER	    7839	81/06/09	2450		    10
+7839	KING	PRESIDENT	    	81/11/17	5000		    10
+7844	TURNER	SALESMAN	7698	81/09/08	1500	0	    30
+7900	JAMES	CLERK	    7698	81/12/03	950		30
+7902	FORD	ANALYST 	7566	81/12/03	3000		    20
+7934	MILLER	CLERK	    7782	82/01/23	1300		    10
+*/
+
+
+--실습 7)
+SELECT * 
+  FROM emp e
+ WHERE e.ENAME = 'ALLEN'
+;
+/*
+EMPNO   ENAME      JOB      MGR     HIREDATE    SAL    COMM   DEPTNO
+---------------------------------------------------------------------
+7499	ALLEN	SALESMAN	7698	81/02/20	1600	300	    30
+*/
+
+
+--실습 8)
+SELECT e.EMPNO AS "사번"
+     , e.ENAME AS "이름"
+     , e.DEPTNO AS "부서번호"
+  FROM emp e
+ WHERE e.DEPTNO = 20
+;
+/*
+사번    이름  부서번호
+----------------------
+7369	SMITH	20
+7566	JONES	20
+7902	FORD	20
+*/
+
+
