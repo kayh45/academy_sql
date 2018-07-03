c583f05d89edf1e13fe967ef4068e516c0043c7b
kayh45 <kayh4545@gmail.com>
2018-07-03 17:29:48 +0900

    수업 시작 커밋 | 실습 15 중간에

--- lab06.sql
+++ lab06.sql
@@ -116,12 +116,196 @@ DEPTNO   DNAME   LOC
 
 
 -- 실습 11)
+
+CREATE TABLE sp_emp
+AS
+SELECT e.* 
+  FROM emp e
+; 
+-- 원본 데이터를 유지하기 위해 새 테이블을 만들었습니다.
+
 CREATE OR REPLACE PROCEDURE sp_chng_emp_comm
-(  v_empno  IN  EMP.EMPNO%TYPE )
+(  v_empno  IN  SP_EMP.EMPNO%TYPE )
 IS
-    v_job   EMP.JOB%TYPE;
-    v_comm  EMP.COMM%TYPE;
+    v_job    SP_EMP.JOB%TYPE;
+    v_comm   SP_EMP.COMM%TYPE;
 BEGIN
-    -- 조건문, UPDATE 처리
+    -- 사번으로 사원을 조회하고 그 사원의 JOB을 가져와 v_job에 저장
+    SELECT e.JOB
+      INTO v_job
+      FROM sp_emp e
+     WHERE e.EMPNO = v_empno
+    ;    
+    -- 조건문을 통해 커미션 값 조정
+    IF      v_job = 'SALESMAN' THEN v_comm := 1000;
+    ELSIF   v_job = 'MANAGER' THEN v_comm := 1500;
+    ELSE    v_comm := 500;
+    END IF
+    ;
+    -- 커미션 값을 UPDATE문을 통해 테이블에 반영 및 커밋
+    UPDATE sp_emp e 
+       SET e.COMM = v_comm
+     WHERE e.EMPNO = v_empno
+    ;
+    COMMIT;
 END sp_chng_emp_comm;
 /
+
+EXEC sp_chng_emp_comm(7499);
+
+SELECT e.EMPNO  AS "사번"
+     , e.ENAME  AS "이름"
+     , e.JOB    AS "직책"
+     , e.COMM   AS "커미션"
+  FROM sp_emp e
+  WHERE e.EMPNO = 7499
+;
+/* 프로시저 실행 전 (7499)
+
+사번    이름    직책      커미션
+--------------------------------
+7499	ALLEN	SALESMAN	300
+*/
+/* 프로시저 실행 후 (7499)
+
+사번    이름    직책      커미션
+--------------------------------
+7499	ALLEN	SALESMAN   1000
+*/
+
+EXEC sp_chng_emp_comm(7566);
+
+SELECT e.EMPNO  AS "사번"
+     , e.ENAME  AS "이름"
+     , e.JOB    AS "직책"
+     , e.COMM   AS "커미션"
+  FROM sp_emp e
+  WHERE e.EMPNO = 7566
+;
+/* 프로시저 실행 전 (7566)
+
+사번    이름    직책      커미션
+--------------------------------
+7566	JONES	MANAGER	 (null)
+*/
+/* 프로시저 실행 후 (7566)
+
+사번    이름    직책      커미션
+--------------------------------
+7566	JONES	MANAGER	  1500
+*/
+
+EXEC sp_chng_emp_comm(7777);
+
+SELECT e.EMPNO  AS "사번"
+     , e.ENAME  AS "이름"
+     , e.JOB    AS "직책"
+     , e.COMM   AS "커미션"
+  FROM sp_emp e
+  WHERE e.EMPNO = 7777
+;
+/* 프로시저 실행 전 (7777)
+
+사번    이름    직책      커미션
+--------------------------------
+7777	J%JONES	CLERK	 (null)
+*/
+/* 프로시저 실행 후 (7777)
+
+사번    이름    직책      커미션
+--------------------------------
+7777	J%JONES	CLERK	  500
+*/
+
+
+-- 실습 12)
+DECLARE
+    v_cnt   NUMBER := 0;
+BEGIN
+    LOOP
+        v_cnt := v_cnt + 1;
+        DBMS_OUTPUT.PUT_LINE(v_cnt);
+        EXIT WHEN v_cnt >= 10;
+    END LOOP
+    ;
+END;
+/
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+PL/SQL 프로시저가 성공적으로 완료되었습니다.
+*/
+
+-- 실습 13)
+DECLARE
+    v_cnt NUMBER := 0;
+BEGIN
+    FOR v_cnt IN REVERSE 1 .. 10 LOOP
+        IF MOD(v_cnt, 2) = 0 
+           THEN DBMS_OUTPUT.PUT_LINE(v_cnt);
+        END IF
+        ;
+    END LOOP
+    ;
+END;
+/
+/*
+10
+8
+6
+4
+2
+
+PL/SQL 프로시저가 성공적으로 완료되었습니다.
+*/
+
+
+-- 실습 14)
+DECLARE 
+    v_cnt NUMBER := 0;
+BEGIN
+    WHILE (v_cnt < 10) LOOP
+        v_cnt := v_cnt + 1;
+        DBMS_OUTPUT.PUT_LINE(v_cnt);
+    END LOOP
+    ;
+END;
+/
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+PL/SQL 프로시저가 성공적으로 완료되었습니다.
+*/
+
+
+-- 실습 15)
+CREATE OR REPLACE FUNCTION fn_emp_sal_avg
+(  v_job        IN  EMP.JOB%TYPE
+ , v_sal_avg)   OUT EMP.SAL%TYPE
+RETURN NUMBER
+IS
+BEGIN
+    SELECT 
+      INTO
+      FROM
+     GROUP BY
+    HAVING
+END;
